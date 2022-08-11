455b41cb220ecd205b6f489475b96da10713446d
Felipe Moreno Borges <19232807+DevCapu@users.noreply.github.com>
2022-08-11 16:50:19 -0300

    vídeo 2.2

--- app/src/androidTest/java/br/com/alura/orgs/ProdutoActivityTest.kt
+++ app/src/androidTest/java/br/com/alura/orgs/ProdutoActivityTest.kt
@@ -1,11 +1,19 @@
 package br.com.alura.orgs
 
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import br.com.alura.orgs.ui.activity.ListaProdutosActivity
 import org.junit.Test
 
 class ProdutoActivityTest {
 
     @Test
     fun deveMostrarONomeDoAplicativo() {
-
+        ActivityScenario.launch(ListaProdutosActivity::class.java)
+        Espresso
+            .onView(ViewMatchers.withText("Orgs"))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
     }
 }
